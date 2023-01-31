723b85f64e62d8b63cbe60aa0922c1eff111064c
evridiky <120931048+evridiky@users.noreply.github.com>
2023-01-31 19:35:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
-const { application } = require('express');
-const { sql } = require('msnodesqlv8');
 
-var Connection = require('tedious').Connection;  
-var config = {  
-server: 'localhost:1433',
-authentication: {
-    type: 'default',
-    options: {
-        userName: 'sa', 
-        password: 'your_password'  
-    }
-},
-options: {
-    encrypt: true,
-    database: 'Apofoitoi UNIPI'  
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// import { BrowserWindow } from "electron";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCP9kuUw6tB-BHrCfYXfKB1lgDu24Swmw",
+  authDomain: "wfmapp-67a1d.firebaseapp.com",
+  databaseURL: "https://wfmapp-67a1d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wfmapp-67a1d",
+  storageBucket: "wfmapp-67a1d.appspot.com",
+  messagingSenderId: "80760163680",
+  appId: "1:80760163680:web:c46cecdc3cb1f7b47f2205",
+  measurementId: "G-H36TDGWGMJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var WFMApp = firebase.database().ref("WFMApp");
+
+document.getElementById("WFMApp").addEventListener("submit",submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    var Day = getElementVal ("Day");
+    var Calls = getElementVal("Calls");
 }
-};  
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
-// If no error, then good to proceed.
-console.log("Connected");  
-});
 
-connection.connect();
+const saveMessages = (Day, Calls) => {
+    var WFMApp = WFMApp.push();
 
-function 
-   executeStatement() {
-      request = new Request("select * from tblStudents", function(err, rowCount) {
-         if(err) {
-            console.log(err);
-         } 
-         else {
-            console.log(rowCount + ' rows');
-         }
-      });
+    newWFMApp.set ({
+        Day: Day,
+        Calls: Calls
+    })
+}
 
-      request.on('row', function(columns) {
-         columns.forEach(function(column) {
-            console.log(column.value);
-         });
-      });
 
-      connection.execSql(request);
-   }
+// let win = null;
+// const createWindow = () => {
+//     win = new BrowserWindow ( { 
+//         width:800,
+//         height:600,
+//         resizable:true,
+//         webPreferences: {
+//             nodeIntegration:true
+//         }
+//     })
+
+//     win.loadFile("index.html");
+
+// };
+
+// app.whenRead().them(createWindow);
